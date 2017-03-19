72c2434366f7569d8e5f28370d4f3a527e9a411c
Joseph <jsadle5@students.towson.edu>
2017-03-18 21:27:29 -0400

    Task 4 - Acceptance Testing
    Tests written based on test requirements. 90%+ statement coverage on
    CoffeeMaker, Recipe, and Inventory.

--- CoffeeMaker/src/edu/towson/cis/cosc442/project4/coffeemaker/CoffeeMakerTest.java
+++ CoffeeMaker/src/edu/towson/cis/cosc442/project4/coffeemaker/CoffeeMakerTest.java
@@ -40,5 +40,83 @@ public class CoffeeMakerTest extends TestCase {
 		assertTrue(cm.editRecipe(r1, newRecipe));
 	}
 	
+	public void testAddInventory1() {
+		assertTrue(cm.addInventory(5, 5, 5, 5));
+	}
+	
+	public void testCheckInventory() {
+		assertNotNull(cm.checkInventory());
+	}
+	
+	public void testPurchaseBeverage1() { //Test successful purchase
+		cm.addRecipe(r1);
+		assertEquals(0, cm.makeCoffee(r1,50));
+	}
+	
+	public void testPurchaseBeverage2() { //Test not enough money inserted
+		cm.addRecipe(r1);
+		assertEquals(49, cm.makeCoffee(r1,49));
+	}
+	
+	public void testPurchaseBeverage3() { //Test not enough inventory
+		Recipe r2 = new Recipe();
+		r2.setName("Coffee");
+		r2.setPrice(50);
+		r2.setAmtCoffee(600);
+		r2.setAmtMilk(1);
+		r2.setAmtSugar(1);
+		r2.setAmtChocolate(0);
+		cm.addRecipe(r2);
+		assertEquals(50, cm.makeCoffee(r2,50));
+	}
+	
+	public void testGetRecipeForName() {
+		cm.addRecipe(r1);
+		assertEquals(r1, cm.getRecipeForName(r1.getName()));
+	}
+	
+	public void testEnoughIngredients() {
+		Recipe r2 = new Recipe();
+		r2.setName("Coffee");
+		r2.setPrice(50);
+		r2.setAmtCoffee(600);
+		r2.setAmtMilk(100);
+		r2.setAmtSugar(100);
+		r2.setAmtChocolate(100);
+		cm.checkInventory().enoughIngredients(r2);
+	}
+	
+	public void testInventorySetters() {
+		Inventory inv = cm.checkInventory();
+		
+		inv.setCoffee(-1);
+		inv.setMilk(-1);
+		inv.setSugar(-1);
+		inv.setChocolate(-1);
+		
+		assertEquals(0, inv.getCoffee());
+		assertEquals(0, inv.getMilk());
+		assertEquals(0, inv.getSugar());
+		assertEquals(0, inv.getChocolate());
+	}
+	
+	public void testInventoryToString() {
+		assertNotNull(cm.checkInventory().toString());
+	}
+	
+	public void testRecipeSetters() {
+		Recipe r2 = new Recipe();
+		r2.setAmtCoffee(-1);
+		r2.setAmtMilk(-1);
+		r2.setAmtSugar(-1);
+		r2.setAmtChocolate(-1);
+		r2.setPrice(-1);
+		
+		assertEquals(0, r2.getAmtCoffee());
+		assertEquals(0, r2.getAmtMilk());
+		assertEquals(0, r2.getAmtSugar());
+		assertEquals(0, r2.getAmtChocolate());
+		assertEquals(0, r2.getPrice());
+	}
 
 }
